38701bb6b74119fbe262c28bb250c25c218aa230
MgdlenaT <93317140+MgdlenaT@users.noreply.github.com>
2022-11-14 14:01:51 +0100

    day1- guess the number

---
+++ day1.py
@@ -0,0 +1,27 @@
+from random import randint
+
+while True:
+    number = randint(1, 256)
+    user_number = False
+    while user_number != number:
+        try:
+            user_number = int(input("Guess the number between 1 to 256: "))
+            if user_number > number:
+                print("Too much, try again")
+            elif user_number < number:
+                print("Too small")
+                continue
+            else:
+                print(f"Congratulations, you guessed it! The number is {number}! ")
+                user_number = True
+                break
+
+        except ValueError:
+            print('Try again, use only an integer number')
+
+    users_decision = input("Do you want to play again? Write 'OK'. If not- press anything: ")
+    if users_decision.upper() == "OK":
+        continue
+    else:
+        print("Thanks for the game")
+        break
